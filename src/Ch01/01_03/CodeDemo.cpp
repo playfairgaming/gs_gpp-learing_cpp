2168cc31888d532db53ef9caabcda7a5eb4d446e
Xeus <playfairgaming@mail.com>
2024-06-10 07:58:57 +0000

    feat: Ch01_03

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -2,14 +2,30 @@
 // Challenge 01_03
 // Console Interaction, by Eduardo Corpeño 
 
+/*
+ * - Print message requesting user's name
+ * - Print a message using that name
+ * */
+
 #include <iostream>
 #include <string>
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
 
+    /* print message requesting user's name */
+    std::string msg { "Please enter your name: " };
+    std::cout << msg;
+
+    /* get user's name */
+    std::string name;                        // declare string variable, name
+    std::cin >> name;                        // assign user input to name
+
+    /* print message using user's name */
+    std::string greeting { "It's nice to meet you " + name + ".\n" };
+    std::cout << greeting;
+
+    // eduardo making space in the output
     std::cout << std::endl << std::endl;
+
     return (0);
 }
