4fbf3ea85afa7aa0304f9d9a50e79dcae68767d0
Xeus <playfairgaming@mail.com>
2024-06-10 08:59:29 +0000

    feat: Ch02_13

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -2,11 +2,22 @@
 // Challenge 02_13
 // Calculate an Average, by Eduardo Corpeño 
 
+/*
+ * - calculate the average in an array of five integers
+ * - just add the values and divide by 5
+ * - store the result in  a float variable
+ * - print the average on screen
+ * - haven't technically learned loops yet 
+ * */
+
 #include <iostream>
 
 int main(){
-    int nums[5] = {1,23,32,24,337};
-    float result; 
+    const int length = 5;
+    int nums[length] = {1,23,32,24,337};
+    float result {
+        (float)(nums[0] + nums[1] + nums[2] + nums[3] + nums[4]) / length
+    }; 
 
     // Write your code here
     
