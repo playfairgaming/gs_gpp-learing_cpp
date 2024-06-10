79e323ba8ac5140b9fff6de263232cccb962ed03
Xeus <playfairgaming@mail.com>
2024-06-10 07:58:12 +0000

    feat: Ch01_02b

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main() {
+    std::cout << "Greetings and salutations Earth.\n\n";
+    return 0;   
+}
+
+/*
+ * The end file has a lot of std::endl while previous sources have suggested the 
+ * use of '\n' as endl actually flushes the buffer while '\n' is just an 
+ * additional line
+ * */
